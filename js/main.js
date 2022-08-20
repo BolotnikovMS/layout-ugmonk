cf705b376b434d272b1c33a584eca81bcc5cf7cf
BolotnikovMS <bolotnikovms@yandex.ru>
2022-08-20 17:04:08 +0500

    Update and mobile adaptive

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     once: false,
     duration: 1000,
   })
+
+  // Mobile nav
+  const navBtn = document.querySelector('.nav__toggle')
+  const menuIcon = document.querySelector('.menu-icon')
+  const nav = document.querySelector('.nav')
+
+  navBtn.addEventListener('click', () => {
+    menuIcon.classList.toggle('menu-icon-active')
+    nav.classList.toggle('nav--mobile')
+    document.body.classList.toggle('no-scroll')
+  })
 })
